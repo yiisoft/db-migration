46eab2a533dda8978e78eedad6820831fe057eae
Alexander Makarov <sam@rmcreative.ru>
2020-05-21 17:44:26 +0300

    Simplify tests config

--- config/tests.php
+++ config/tests.php
@@ -6,25 +6,19 @@ use Psr\Container\ContainerInterface;
 use Psr\EventDispatcher\EventDispatcherInterface;
 use Psr\EventDispatcher\ListenerProviderInterface;
 use Psr\Log\LoggerInterface;
-use Psr\Log\LogLevel;
 use Psr\SimpleCache\CacheInterface as SimpleCacheInterface;
 use Yiisoft\Aliases\Aliases;
 use Yiisoft\Cache\ArrayCache;
 use Yiisoft\Cache\Cache;
 use Yiisoft\Cache\CacheInterface;
-use Yiisoft\Factory\Definitions\Reference;
 use Yiisoft\Db\Connection\Connection;
 use Yiisoft\Db\Connection\ConnectionPool;
 use Yiisoft\EventDispatcher\Dispatcher\Dispatcher;
 use Yiisoft\EventDispatcher\Provider\Provider;
-use Yiisoft\Db\Helper\Dsn;
 use Yiisoft\Log\Logger;
 use Yiisoft\Log\Target\File\FileRotator;
 use Yiisoft\Log\Target\File\FileRotatorInterface;
-use Yiisoft\Log\Target\File\FileTarget;
 use Yiisoft\Profiler\Profiler;
-use Yiisoft\View\Theme;
-use Yiisoft\View\View;
 
 return [
     ContainerInterface::class => static function (ContainerInterface $container) {
@@ -38,17 +32,8 @@ return [
         '@views' => '@root/resources/views'
     ],
 
-    ListenerProviderInterface::class => [
-        '__class' => Provider::class,
-    ],
-
-    EventDispatcherInterface::class => [
-        '__class' => Dispatcher::class,
-        '__construct()' => [
-           'listenerProvider' => Reference::to(ListenerProviderInterface::class)
-        ],
-    ],
-
+    ListenerProviderInterface::class => Provider::class,
+    EventDispatcherInterface::class => Dispatcher::class,
     SimpleCacheInterface::class => ArrayCache::class,
 
     CacheInterface::class => Cache::class,
@@ -60,42 +45,7 @@ return [
         ],
     ],
 
-    LoggerInterface::class => static function (ContainerInterface $container) {
-        $aliases = $container->get(Aliases::class);
-        $fileRotator = $container->get(FileRotatorInterface::class);
-
-        $fileTarget = new FileTarget(
-            $aliases->get('@runtime/logs/app.log'),
-            $fileRotator
-        );
-
-        $fileTarget->setLevels(
-            [
-                LogLevel::EMERGENCY,
-                LogLevel::ERROR,
-                LogLevel::WARNING,
-                LogLevel::INFO,
-                LogLevel::DEBUG
-            ]
-        );
-
-        return new Logger([
-            'file' => $fileTarget,
-        ]);
-    },
-
-    Profiler::class => static function (ContainerInterface $container) {
-        return new Profiler($container->get(LoggerInterface::class));
-    },
-
-    View::class => function (ContainerInterface $container) {
-        $aliases = $container->get(Aliases::class);
-        $eventDispatcher = $container->get(EventDispatcherInterface::class);
-        $theme = $container->get(Theme::class);
-        $logger = $container->get(LoggerInterface::class);
-
-        return new View($aliases->get('@migration'), $theme, $eventDispatcher, $logger);
-    },
+    LoggerInterface::class => Logger::class,
 
     Connection::class => static function (ContainerInterface $container) {
         $aliases = $container->get(Aliases::class);
