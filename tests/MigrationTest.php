12a6b709211a7c3e4be4bc26837cd359f121513f
eXeCUT <sexecut@gmail.com>
2020-09-03 20:12:28 +0300

    cherry-pick merge

--- tests/MigrationTest.php
+++ tests/MigrationTest.php
@@ -1,33 +1,15 @@
 <?php
 namespace Yiisoft\Yii\Db\Migration\Tests;
 
-use Yiisoft\Yii\Db\Migration\Migration;
-use Yiisoft\Db\Connection\Connection;
-
-/**
- * @group paths
- */
 final class MigrationTest extends TestCase
 {
     public function testExecute() {
         $db = $this->db;
-        $migration = $this->getMigration($db);
+        $db->createCommand('CREATE TABLE test_table (id int)')->execute();
+
+        $migration = new Migration($db);
 
         $migration->execute('DROP TABLE migration');
         $this->assertEmpty($db->getSchema()->getTableSchema('migration'));
-    }
-
-    /**
-     * Returns Migration mock object
-     * @param Connection $db
-     * @return Migration
-     */
-    private function getMigration(Connection $db): Migration
-    {
-        $migration = $this->getMockBuilder(Migration::class)
-            ->setConstructorArgs([$db])
-            ->onlyMethods(['up', 'down'])
-            ->getMock();
-        return $migration;
     }
 }
