deec330f8d271d0aa7ef995eb815244ea02adb6b
eXeCUT <sexecut@gmail.com>
2020-09-03 20:12:28 +0300

    Added migration stub class

---
+++ tests/Migration.php
@@ -0,0 +1,19 @@
+<?php
+namespace Yiisoft\Yii\Db\Migration\Tests;
+
+use \Yiisoft\Yii\Db\Migration\Migration as BaseMigration;
+
+/**
+ * Stub Migration class
+ * @package Yiisoft\Yii\Db\Migration\Tests
+ */
+class Migration extends BaseMigration
+{
+    public function up(): void
+    {
+    }
+
+    public function down(): void
+    {
+    }
+}
--- tests/MigrationTest.php
+++ tests/MigrationTest.php
@@ -1,9 +1,6 @@
 <?php
 namespace Yiisoft\Yii\Db\Migration\Tests;
 
-use Yiisoft\Yii\Db\Migration\Migration;
-use Yiisoft\Db\Connection\Connection;
-
 /**
  * @group paths
  */
@@ -12,23 +9,10 @@ final class MigrationTest extends TestCase
     public function testExecute() {
         $db = $this->db;
         $db->createCommand('CREATE TABLE test_table (id int)')->execute();
-        $migration = $this->getMigration($db);
+
+        $migration = new Migration($db);
 
         $migration->execute('DROP TABLE test_table');
         $this->assertEmpty($db->getSchema()->getTableSchema('test'));
-    }
-
-    /**
-     * Returns Migration mock object
-     * @param Connection $db
-     * @return Migration
-     */
-    private function getMigration(Connection $db): Migration
-    {
-        $migration = $this->getMockBuilder(Migration::class)
-            ->setConstructorArgs([$db])
-            ->onlyMethods(['up', 'down'])
-            ->getMock();
-        return $migration;
     }
 }
