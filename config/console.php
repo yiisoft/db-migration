42a6d722133c273e6607b25f00ed4922a2ebf656
Wilmer Arambula <terabytesoftw@gmail.com>
2020-12-29 19:30:52 -0300

    Move config view to yiisoft/view.

--- config/console.php
+++ config/console.php
@@ -8,13 +8,6 @@ use Yiisoft\View\View;
 /** @var array $params */
 
 return [
-    View::class => [
-        '__class' => View::class,
-        '__construct()' => [
-            'basePath' => fn (Aliases $aliases) => $aliases->get($params['yiisoft/view']['basePath']),
-        ],
-    ],
-
     MigrationService::class => [
         '__class' => MigrationService::class,
         'createNamespace()' => [fn () => $params['yiisoft/yii-db-migration']['createNamespace']],
