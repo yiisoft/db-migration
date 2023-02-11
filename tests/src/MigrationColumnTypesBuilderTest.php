caeb2dc62c1afd2e5c9d3703c1bcfdb68f2004db
darkdef <darkdef@gmail.com>
2023-02-11 20:48:59 +0300

    test fix

---
+++ tests/src/MigrationColumnTypesBuilderTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Yii\Db\Tests\Migration;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @group db
+ *
+ * @psalm-suppress PropertyNotSetInConstructor
+ */
+final class MigrationColumnTypesBuilderTest extends TestCase
+{
+}
