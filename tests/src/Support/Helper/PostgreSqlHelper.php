63f4b50c19746052039cb9d39b3511e24d6937f6
Sergei Predvoditelev <sergei@predvoditelev.ru>
2023-03-31 18:11:43 +0300

    Update PostgreSqlHelper.php

--- tests/src/Support/Helper/PostgreSqlHelper.php
+++ tests/src/Support/Helper/PostgreSqlHelper.php
@@ -23,7 +23,7 @@ final class PostgreSqlHelper
     {
         $config ??= new ContainerConfig();
 
-        return new SimpleContainer(
+        $container = new SimpleContainer(
             [
                 LoggerInterface::class => new NullLogger(),
                 SchemaCache::class => new SchemaCache(new MemorySimpleCache()),
@@ -53,6 +53,7 @@ final class PostgreSqlHelper
                 }
             }
         );
+        return $container;
     }
 
     public static function clearDatabase(ContainerInterface $container): void
