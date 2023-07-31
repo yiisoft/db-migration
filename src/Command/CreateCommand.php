4cb69ecedf94c8194c2db80f05e0a689470c8a7a
Wilmer Arambula <terabytesoftw@gmail.com>
2023-07-31 09:20:26 -0400

    Fix `$transactional` option.

--- src/Command/CreateCommand.php
+++ src/Command/CreateCommand.php
@@ -139,7 +139,7 @@ final class CreateCommand extends Command
         $namespace = $input->getOption('namespace');
 
         /** @var bool */
-        $transactional = $input->getOption('transactional') === '' ? true : false;
+        $transactional = $input->getOption('transactional') !== false;
 
         if (!preg_match('/^[\w\\\\]+$/', $name)) {
             $io->error(
