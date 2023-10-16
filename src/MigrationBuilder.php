198636d512882740299aa43844c32a388ce225f9
Tigrov <rrr-r@ya.ru>
2023-10-16 20:03:45 +0700

    to lowercase

--- src/MigrationBuilder.php
+++ src/MigrationBuilder.php
@@ -490,7 +490,7 @@ final class MigrationBuilder extends AbstractMigrationBuilder
             'Create'
             . ($indexType !== null ? ' ' . $indexType : '')
             . " index $name on $table (" . implode(',', (array) $columns) . ')'
-            . ($indexMethod !== null ? ' USING ' . $indexMethod : '')
+            . ($indexMethod !== null ? ' using ' . $indexMethod : '')
         );
         $this->db->createCommand()->createIndex($table, $name, $columns, $indexType, $indexMethod)->execute();
         $this->endCommand($time);
