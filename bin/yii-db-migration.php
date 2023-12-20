bc287badca06e39cf44fe751a88d97107579db57
Sergei Predvoditelev <sergei@predvoditelev.ru>
2023-12-20 13:06:36 +0300

    improve console utility

---
+++ bin/yii-db-migration.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'db' => null,
+    'useTablePrefix' => true,
+    'container' => null,
+    'historyTable' => '{{%migration}}',
+    'migrationNameLimit' => 180,
+    'maxSqlOutputLength' => null,
+    'createNamespace' => '',
+    'createPath' => '',
+    'updateNamespaces' => [],
+    'updatePaths' => [],
+];
