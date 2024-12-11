0c0d9685040c9e36f12a668d928f57b672cf2e58
Tigrov <rrr-r@ya.ru>
2024-12-11 18:56:00 +0700

    Fix MSSQL tests

--- tests/Support/Factory/MssqlFactory.php
+++ tests/Support/Factory/MssqlFactory.php
@@ -33,7 +33,7 @@ final class MssqlFactory
                 return match ($id) {
                     ConnectionInterface::class => new MssqlConnection(
                         new MssqlDriver(
-                            'sqlsrv:Server=127.0.0.1,1433;Database=yiitest',
+                            'sqlsrv:Server=127.0.0.1,1433;Database=yiitest;Encrypt=no',
                             'SA',
                             'YourStrong!Passw0rd',
                         ),
