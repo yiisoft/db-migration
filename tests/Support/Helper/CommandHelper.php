62452e8930dafc18ecf81b3cf03a012d210a6a4d
Tigrov <rrr-r@ya.ru>
2025-08-28 14:32:05 +0700

    Fix tests

--- tests/Support/Helper/CommandHelper.php
+++ tests/Support/Helper/CommandHelper.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Yiisoft\Db\Migration\Tests\Support\Helper;
 
 use Psr\Container\ContainerInterface;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Tester\CommandTester;
 use Yiisoft\Yii\Console\Application;
 use Yiisoft\Yii\Console\CommandLoader;
@@ -12,20 +13,21 @@ use Yiisoft\Yii\Console\CommandLoader;
 final class CommandHelper
 {
     /**
-     * @psalm-param class-string $commandClass
+     * @psalm-param class-string<Command> $commandClass
      */
     public static function getCommandTester(ContainerInterface $container, string $commandClass): CommandTester
     {
+        $commandName = $commandClass::getDefaultName();
         $application = new Application();
         $application->setCommandLoader(
             new CommandLoader(
                 $container,
-                ['test-command' => $commandClass]
+                [$commandName => $commandClass]
             )
         );
 
         return new CommandTester(
-            $application->find('test-command')
+            $application->find($commandName)
         );
     }
 }
