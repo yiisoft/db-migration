e13e39ff9aa0670b662e15ec10f76bbfc0e901ba
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-12-20 08:32:34 +0300

    fix

--- tests/Support/Helper/CommandHelper.php
+++ tests/Support/Helper/CommandHelper.php
@@ -4,11 +4,16 @@ declare(strict_types=1);
 
 namespace Yiisoft\Db\Migration\Tests\Support\Helper;
 
+use LogicException;
 use Psr\Container\ContainerInterface;
+use ReflectionClass;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Tester\CommandTester;
 use Yiisoft\Yii\Console\Application;
 use Yiisoft\Yii\Console\CommandLoader;
+
+use function sprintf;
 
 final class CommandHelper
 {
@@ -17,7 +22,14 @@ final class CommandHelper
      */
     public static function getCommandTester(ContainerInterface $container, string $commandClass): CommandTester
     {
-        $commandName = $commandClass::getDefaultName();
+        if ($attribute = (new ReflectionClass($commandClass))->getAttributes(AsCommand::class)) {
+            $commandName = $attribute[0]->newInstance()->name;
+        } else {
+            throw new LogicException(
+                sprintf('The command "%s" does not have the AsCommand attribute.', $commandClass),
+            );
+        }
+
         $application = new Application();
         $application->setCommandLoader(
             new CommandLoader(
